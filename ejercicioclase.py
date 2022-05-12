1da7d815f69c547fc05a00265591868ebde6fc4a
JHONATANGARCIAXD <jhonatangarciacolegioguanenta.edu.co>
2022-05-12 18:01:58 -0500

    Desarrololo: Ciclo While

---
+++ ejercicioclase.py
@@ -0,0 +1,23 @@
+# Ejercicio Clase
+
+suma=0
+j=1
+print("Dame el valor de n.\n")
+
+# input 
+
+n=int(input())
+
+# processing 
+
+while(j<=n):
+    suma=suma+j
+    j=j+1
+
+# ouput
+
+print("La suma es:",suma)
+
+
+
+
